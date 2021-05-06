f93d3a68dff7647ec9bb4de12b9e6dc54143fac0
Shubham Tiwary <shubhut17@gmail.com>
2021-05-06 22:35:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useTranslation } from "react-i18next";
+import "./App.css";
+import i18next from 'i18next';
 
 function App() {
+  const { t } = useTranslation();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>i18next.changeLanguage('en')}>English</button>
+      <button onClick={()=>i18next.changeLanguage('hi')}>Hindi</button>
+      <h1>{t("main_head")}</h1>
+      <p>{t("main_para")}</p>
     </div>
   );
 }
